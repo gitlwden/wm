a7a2ca952d14475785dc0c421858e602845ca86e
vl <jlulwd@gmail.com>
2026-06-08 19:49:07 +0800

    revert: switch intelligence/market RPC back to edge runtime (nodejs crashes on Vercel)

--- api/market/v1/[rpc].ts
+++ api/market/v1/[rpc].ts
@@ -1,4 +1,4 @@
-export const config = { runtime: 'nodejs' };
+export const config = { runtime: 'edge', regions: ['iad1', 'lhr1', 'fra1', 'sfo1'] };
 
 import { createDomainGateway, serverOptions } from '../../../server/gateway';
 import { createMarketServiceRoutes } from '../../../src/generated/server/worldmonitor/market/v1/service_server';
